27cab13ab373bd5076469c7b878bee377fc08730
Lohit Vankineni <Lohit@lohits-mbp.home>
2017-05-17 22:57:09 -0700

    updated stuff

--- src/app/browseposts/browseposts.component.ts
+++ src/app/browseposts/browseposts.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods, FirebaseApp } from 'angularfire2';
+import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import { UploadService } from '../uploads/shared/upload.service';
+import { Upload } from '../uploads/shared/upload';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-browseposts',
@@ -6,8 +12,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./browseposts.component.css']
 })
 export class BrowsepostsComponent implements OnInit {
+  auth: any;
+  displayName: any;
 
-  constructor() { }
+  constructor(@Inject(FirebaseApp) firebaseApp: any, public af: AngularFire, public db: AngularFireDatabase,  private router: Router,private upSvc: UploadService) {
+    this.af.auth.subscribe(auth => {
+      if (auth) {
+        this.displayName = auth.auth.displayName;
+        this.auth = auth;
+      }
+    });
+  }
 
   ngOnInit() {
   }
--- src/app/submission/submission.component.ts
+++ src/app/submission/submission.component.ts
@@ -11,8 +11,35 @@ import { Router } from '@angular/router';
   styleUrls: ['./submission.component.css']
 })
 export class SubmissionComponent implements OnInit {
+  selectedFiles: FileList;
+  currentUpload: Upload;
+  auth: any;
+  displayName: any;
+  classFile: any;
+  codeFile: any;
 
-  constructor() { }
+  constructor(@Inject(FirebaseApp) firebaseApp: any, public af: AngularFire, public db: AngularFireDatabase,  private router: Router,private upSvc: UploadService) {
+    this.af.auth.subscribe(auth => {
+      if (auth) {
+        this.displayName = auth.auth.displayName;
+        this.auth = auth;
+      }
+    });
+  }
+
+  detectFiles(event) {
+    this.selectedFiles = event.target.files;
+  }
+
+  uploadCodeFile() {
+    let file = this.selectedFiles.item(0);
+    this.currentUpload = new Upload(file);
+    this.upSvc.pushUpload(this.currentUpload, function(key){
+
+      this.codeFile = key;
+    });
+  }
+
 
   ngOnInit() {
   }
